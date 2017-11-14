ed51f9009fe2a97f499c7cefe956d54ee03bbc90
Cédric Malard <c.malard-git@valdun.net>
2017-11-14 15:35:50 +0100

    Refactor to ES6 & functions

--- meetup-friends.user.js
+++ meetup-friends.user.js
@@ -9,75 +9,99 @@
 // @require        http://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
 
+/* eslint-env greasemonkey, jquery */
+
 this.$ = this.jQuery = jQuery.noConflict(true);
 
-var friends = [
+const friends = [
   // 123456789, // ID of a friend
 ];
 
-// Display position in wait list
-$('#rsvp-list-waitlist li').each(function (i, elt) {
-  $('h5', this).prepend('#' + (i + 1) + ' - ');
-});
-
-// Display wait list length
-$('.event-attendees h3').after('<h4>' + $('#rsvp-list-waitlist li').length + ' waiting</h4>');
-
-// Highlight friends
-var friends_list = {};
-var colors = {
+const colors = {
   list: '#0fd9a3',
   waitlist: '#353e48',
   no: '#ed1c40',
 };
 
-var $first_attendee = $('#rsvp-list').children().first();
-if (!$first_attendee.hasClass('memberinfo-widget')) {
-  $first_attendee = $first_attendee.next();
+const displaySections = getDisplaySections();
+
+displayWaitPosition();
+displayWaitLength();
+
+displayFriends();
+autoUpdate();
+
+
+/** **************************************************************************
+ *  Functions
+ */
+function autoUpdate() {
+  $('.nav-appendPager').click(() => {
+    window.setTimeout(() => {
+      displayFriends();
+    }, 1000);
+  });
 }
 
-function createLists() {
-  for (var status in colors) {
-    friends_list['$' + status] = $('<div/>')
-      .css({
-        'box-shadow': '0 0 0 10px ' + colors[status],
-        'margin-bottom': '20px'
-      })
-      .insertBefore($first_attendee);
-  }
-}
-
-function updateFriendsList() {
-  for each ($list in friends_list) {
+function displayFriends() {
+  Object.values(displaySections).forEach(($list) => {
     $list.empty().show();
-  }
+  });
 
-  $.each(friends, function(i, id) {
-    var $friend = $('#rsvp_' + id);
+  friends.forEach((id) => {
+    const $friend = $(`#rsvp_${id}`);
     if (!$friend.length) {
       return;
     }
 
-    var friend_status = $friend.parent().attr('id').replace(/.*-/, '');
-    $friend.clone().appendTo(friends_list['$' + friend_status]);
+    const friendStatus = $friend.parent().attr('id').replace(/.*-/, '');
+    $friend.clone().appendTo(displaySections[`$${friendStatus}`]);
     $friend.css({
       'background-color': colors.list,
-      'box-shadow': '10px 0 0 0 ' + colors.list + ', -10px 0 0 0 ' + colors.list
+      'box-shadow': `10px 0 0 0 ${colors.list}, -10px 0 0 0 ${colors.list}`,
     });
   });
 
-  for each ($list in friends_list) {
+  Object.values(displaySections).forEach(($list) => {
     if (!$list.children().length) {
       $list.hide();
     }
-  }
+  });
 }
 
-createLists();
-updateFriendsList();
+function displayWaitLength() {
+  const waitLength = $('#rsvp-list-waitlist li').length;
+  $('.event-attendees h3')
+    .after(`<h4>${waitLength} waiting</h4>`);
+}
 
-$('.nav-appendPager').click(function() {
-  window.setTimeout(function() {
-    updateFriendsList();
-  }, 1000);
-});
+function displayWaitPosition() {
+  $('#rsvp-list-waitlist li').each((i) => {
+    $('h5', this).prepend(`#${i + 1} - `);
+  });
+}
+
+function get$firstAttendee() {
+  let $attendee = $('#rsvp-list').children().first();
+  if (!$attendee.hasClass('memberinfo-widget')) {
+    $attendee = $attendee.next();
+  }
+
+  return $attendee;
+}
+
+function getDisplaySections() {
+  const $firstAttendee = get$firstAttendee();
+  const sections = {};
+
+  for (let status in colors) {
+    sections[`$${status}`] = $('<div/>')
+      .css({
+        'box-shadow': `0 0 0 10px ${colors[status]}`,
+        'margin-bottom': '20px',
+      })
+      .insertBefore($firstAttendee);
+  }
+
+  return sections;
+}
